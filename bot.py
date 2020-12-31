618803c9a45c19418a98099981c4156ef81e635c
Nataly Novak <miss.novak137@yandex.com>
2020-12-31 13:03:27 +0300

    removing command message

--- bot.py
+++ bot.py
@@ -25,18 +25,22 @@ TOKEN = os.getenv('TOKEN')
 bot = commands.Bot(command_prefix='/')
 
 
-@bot.command(name='quote', help = "generates random quotes with translation")
+@bot.command(name='quote', help = "generates random quotes with translation", pass_context=True)
 async def cookin(ctx):
     response = randomquote(conn)
     await ctx.send(response)
+    await bot.delete_message(ctx.message)
 
 
-@bot.command(name='add' , help='Adds a quote. Use quotes around both quote and translation')
+
+@bot.command(name='add' , help='Adds a quote. Use quotes around both quote and translation', pass_context=True)
 async def itl(ctx,language, line, trans =""):
     addquote(conn,language,line,trans)
+    await bot.delete_message(ctx.message)
 
-@bot.command(name='del' , help='deletes last quote')
+@bot.command(name='del' , help='deletes last quote', pass_context=True)
 async def de(ctx):
     removelast(conn)
+    await bot.delete_message(ctx.message)
 
 bot.run(TOKEN)
