97fded2969ccb30f5e5a542514f647379aa67313
Nataly Novak <miss.novak137@yandex.com>
2020-12-31 14:13:43 +0300

    Trying to deal with deleting messages #2

--- bot.py
+++ bot.py
@@ -29,18 +29,18 @@ bot = commands.Bot(command_prefix='/')
 async def cookin(ctx):
     response = randomquote(conn)
     await ctx.send(response)
-    await ctx.message.delete
+    await ctx.message.delete()
 
 
 
 @bot.command(name='add' , help='Adds a quote. Use quotes around both quote and translation', pass_context=True)
 async def itl(ctx,language, line, trans =""):
     addquote(conn,language,line,trans)
-    await ctx.message.delete
+    await ctx.message.delete()
 
 @bot.command(name='del' , help='deletes last quote', pass_context=True)
 async def de(ctx):
     removelast(conn)
-    await ctx.message.delete
+    await ctx.message.delete()
 
 bot.run(TOKEN)
