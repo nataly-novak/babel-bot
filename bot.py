7dc974765cab2d84b700624c6fb31864572b3b68
Nataly Novak <miss.novak137@yandex.com>
2021-01-07 15:59:07 +0300

    checking the reaction - beginning - fix - added await - fixed?

--- bot.py
+++ bot.py
@@ -112,7 +112,7 @@ async def on_message(message):
     for i in emo:
         print(i)
         em = i[1:-1]
-        emoji = get(bot.emojis(), name=em)
+        emoji = get(bot.emojis, name=em)
         await message.add_reaction(emoji)
     await bot.process_commands(message)
 
