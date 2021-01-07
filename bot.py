f2a88b155437cd3e5bf757f07453f723e540a54c
Nataly Novak <miss.novak137@yandex.com>
2021-01-07 15:01:11 +0300

    checking the reaction - beginning - fix - added await

--- bot.py
+++ bot.py
@@ -107,6 +107,7 @@ async def on_message(message):
     chan = message.channel.id
     line = message.content
     print(getreaction(conn,line,chan))
+    await client.process_commands(message)
 
 
 
