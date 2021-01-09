8f49ccd4363aa5c1b21b3d204148cf90ead4d88e
Nataly Novak <miss.novak137@yandex.com>
2021-01-09 16:59:14 +0300

    removed seconds

--- timework.py
+++ timework.py
@@ -8,7 +8,7 @@ def toUTC(date, time, zone):
     naive_datetime = datetime.datetime.strptime(line, "%Y-%m-%d %H:%M:%S")
     local_datetime = local_time.localize(naive_datetime, is_dst=None)
     utc_datetime = local_datetime.astimezone(pytz.utc)
-    conversion = str(utc_datetime).split(sep="+")[0]+" UTC"
+    conversion = (str(utc_datetime).split(sep="+")[0]).rsplit(sep=":",maxsplit=1)[0]+" UTC"
     return conversion
 
 
@@ -25,7 +25,7 @@ def toLocal(date, time, zone):
     naive_datetime = datetime.datetime.strptime(line, "%Y-%m-%d %H:%M:%S")
     utc_datetime = utc_time.localize(naive_datetime, is_dst=None)
     local_datetime = utc_datetime.astimezone(local_time)
-    conversion = str(local_datetime).split(sep="+")[0]+" "+ zone
+    conversion = str((local_datetime).split(sep="+")[0]).rsplit(sep=":",maxsplit=1)[0]+" "+ zone
     return conversion
 
 
