39f8d47ce5bdef6bbac2b2daa72664c541c5c4ea
Nataly Novak <miss.novak137@yandex.com>
2021-01-11 16:04:35 +0300

    adding reacting to language roles

--- bot.py
+++ bot.py
@@ -231,6 +231,7 @@ async def on_message(message):
 
 @bot.event
 async def on_member_update(before, after):
+    print("newrole")
     print(len(before.roles))
     print(len(after.roles))
     if len(before.roles) < len(after.roles):
